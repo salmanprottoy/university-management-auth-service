5045ff1f462b6dadc4ae74e69888dda980441485
salmanprottoy <salman.prottoy@gmail.com>
2023-06-04 01:36:37 +0600

    # Info
    - setup express, mongoose, cors, parser

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
+const app: Application = express();
+
+app.use(cors());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//routes
+app.get("/", (req: Request, res: Response) => {
+  res.send("Working successfully!");
+});
+
+export default app;
