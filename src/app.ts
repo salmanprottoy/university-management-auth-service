dc68a21af42664943b9c72848fd8e264826708c4
salmanprottoy <salman.prottoy@gmail.com>
2023-06-04 21:46:11 +0600

    # Feature
    - create user service

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
 
 const app: Application = express()
 
@@ -8,6 +9,9 @@ app.use(cors())
 //parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// Application router
+app.use('/api/v1/users/', usersRouter)
 
 //routes
 app.get('/', (req: Request, res: Response) => {
---
+++ src/app/modules/users/users.utils.ts
@@ -0,0 +1,15 @@
+import { User } from './users.model'
+
+export const findLastUserId = async () => {
+  const lastUserId = await User.findOne({}, { id: 1, _id: 0 })
+    .sort({ createdAt: -1 })
+    .lean()
+  return lastUserId?.id
+}
+
+export const generateUserId = async () => {
+  const currentId = (await findLastUserId()) || (0).toString().padStart(6, '0')
+
+  const incrementedId = (parseInt(currentId) + 1).toString().padStart(6, '0')
+  return incrementedId
+}
